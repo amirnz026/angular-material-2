6903e51ae4388484a7874b7c89baa1a836997867
Amir Hosein Nazari <czhablog@gmail.com>
2022-11-21 15:28:24 +0330

    feat

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
-const MaterialComponents = [MatButtonModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
